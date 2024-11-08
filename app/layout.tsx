151bcf28c69193a2e8d7b801d0f154e1d856940c
Sinan Alagöz <sinanalagoz999@gmail.com>
2024-11-09 01:22:19 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import PrelineScript from "@/app/components/PrelineScript";
-import PrelineNavbar from "@/app/components/PrelineNavbar";
 import PrelineFooter from "@/app/components/PrelineFooter";
+import FlexiWindNavbar from "@/app/components/FlexiWindNavbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PrelineNavbar/>
+        <FlexiWindNavbar/>
         {children}
         <PrelineFooter/>
       </body>
