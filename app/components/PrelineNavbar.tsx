f5bfa0d717cb74e3bea5db5cfc9978c0d5287a05
Sinan Alagöz <sinanalagoz999@gmail.com>
2024-12-30 10:37:00 +0300

    commit

--- app/components/PrelineNavbar.tsx
+++ app/components/PrelineNavbar.tsx
@@ -68,11 +68,17 @@ function PrelineNavbar() {
                     aria-labelledby="hs-navbar-example-collapse"
                 >
                     <div
-                        className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+                        className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5 p-2">
                         <Link href="/"
-                              className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500">Anasayfa</Link>
+                              className={`${playfairDisplay.className} font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500 text-base`}
+                        >
+                            Anasayfa
+                        </Link>
                         <Link href="/hakkimda"
-                              className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500">Hakkımda</Link>
+                              className={`${playfairDisplay.className} font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500 text-base`}
+                        >
+                            Hakkımda
+                        </Link>
                         {/*<div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] ">
                             <button
                                 id="hs-navbar-example-dropdown"
